372f61fdb45d0b12f156620aac5a148b153de82e
jonydaimary <45644556+jonydaimary@users.noreply.github.com>
2018-12-22 12:33:25 +0530

    Update marcos.py

--- marcos.py
+++ marcos.py
@@ -14,7 +14,7 @@ from discord import Game, Embed, Color, Status, ChannelType
 
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
-client = Bot(description="DarkBot Bot is best", command_prefix=">", pm_help = True)
+client = Bot(description="DarkBot Bot is best", command_prefix="!!", pm_help = True)
 client.remove_command('help')
 
 
